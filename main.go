d7dac0da0807d37860bac717caccdb1c8e87694f
Tawanda M Nyoni <tmnyoni@outlook.com>
2025-02-03 11:32:37 +0200

    feat: add func to get repos

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/codecommit"
 )
 
+type Codecommit struct {
+	client *codecommit.Client
+}
+
+func CodecommitClient() Codecommit {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("Unable to load AWS SDK config, %v", err)
+	}
+
+	csc := codecommit.NewFromConfig(cfg)
+	return Codecommit{client: csc}
+}
+
+func (c *Codecommit) GetRepos() (codecommit.ListRepositoriesOutput, error) {
+	repos, err := c.client.ListRepositories(context.TODO(), &codecommit.ListRepositoriesInput{})
+	if err != nil {
+		log.Fatalf("Unable to list repositories, %v", err)
+		return codecommit.ListRepositoriesOutput{}, err
+	}
+	return *repos, nil
+}
+
 func main() {
-	fmt.Println("Hello world")
+	cc := CodecommitClient()
+
+	repos, err := cc.GetRepos()
+	if err != nil {
+		log.Fatalf("Unable to list repositories, %v", err)
+	}
+
+	for _, repo := range repos.Repositories {
+		log.Println(*repo.RepositoryName)
+	}
 }
